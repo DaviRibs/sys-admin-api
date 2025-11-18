e81afcd283bddca74e6204450e085f2fb6918ed0
Daviribs <davirb21@gmail.com>
2025-11-18 12:54:15 -0300

    update auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,7 +5,10 @@ const router = express.Router()
 const authController = require("../controllers/auth")
 
 const authMiddleware = require("../middlewares/auth")
+const { authToken } = require("../middlewares/authtoken")
 
 router.post("/login", authMiddleware.validateLogin, authController.login)
+
+router.get("/profile", authToken(), authController.profile)
 
 module.exports = router
