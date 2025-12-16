31a7dc1bc4ff54024b8bab9e1fc76663f62671f2
Daviribs <davirb21@gmail.com>
2025-12-16 13:10:48 -0300

    update productsjs

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,18 +1,30 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 
-const productsController = require("../controllers/products")
+const productsController = require('../controllers/products')
 
-const productsMiddewares = require("../middlewares/products")
-const { authToken } = require("../middlewares/authtoken")
-
+const productsMiddewares = require('../middlewares/products')
+const { authToken } = require('../middlewares/authtoken')
+const axios = require('axios')
 router.post(
-  "/products",
-  authToken(["seller", "admin"]),
+  '/products',
+  authToken(['seller', 'admin']),
   productsMiddewares.validateInsertProduct,
   productsController.insertProduct
 )
 
-router.get("/products", productsController.getAllProducts)
+router.get('/products', productsController.getAllProducts)
+router.post('/email', async (req, res) => {
+  const api = axios.create({
+    baseURL: 'http://localhost:4505',
+    headers: {
+      token: 'Habibs',
+    },
+  })
+  await api.get()
 
+  return res.send({
+    message: 'Email enviado com sucesso!',
+  })
+})
 module.exports = router
